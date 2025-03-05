fb5ff99b8f7559815434c109d505c12c141510da
Dan <git@danharrin.com>
2025-03-05 09:26:25 +0000

    Merge pull request #15777 from binaryfire/add-modal-id-data-attribute

--- resources/views/components/modal/index.blade.php
+++ resources/views/components/modal/index.blade.php
@@ -93,6 +93,7 @@
     @if ($id)
         x-on:{{ $closeEventName }}.window="if ($event.detail.id === '{{ $id }}') close()"
         x-on:{{ $openEventName }}.window="if ($event.detail.id === '{{ $id }}') open()"
+        data-fi-modal-id="{{ $id }}"
     @endif
     x-trap.noscroll{{ $autofocus ? '' : '.noautofocus' }}="isOpen"
     x-bind:class="{
